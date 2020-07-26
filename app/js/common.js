ab06a7f1ca642292752c4a5c930dfbd917d84d2a
viktoriazaharova <zaharova181114@gmail.com>
2020-07-26 13:16:23 +0300

    Initial commit

---
+++ app/js/common.js
@@ -0,0 +1,10 @@
+$('[data-fancybox="gallery"]').fancybox({
+    thumbs          : false,
+    arrows          : true,
+    infobar: false,
+    buttons: [
+        "close"
+    ],
+});
+
+$('[name=phone]').mask("+7(999) 999-99-99");
