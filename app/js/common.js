c664f02f202b6e197e0144419fc258e37b3b387f
viktoriazaharova <zaharova181114@gmail.com>
2020-08-10 13:33:33 +0300

    edit

--- app/js/common.js
+++ app/js/common.js
@@ -8,3 +8,36 @@ $('[data-fancybox="gallery"]').fancybox({
 });
 
 $('[name=phone]').mask("+7(999) 999-99-99");
+
+
+$('.about-content__box').each(function () {
+
+    var heightText = $(this).find('.box-text').height();
+
+    if (heightText > 384) {
+
+        $(this).addClass('collapsed').append('<a href="#" class="links-load">Читать далее..</a>');
+    }
+
+});
+
+$('.links-load').on('click', function(e){
+    e.preventDefault();
+
+    var
+        $this = $(this),
+        content = $(this).parents('.about-content__box');
+
+
+    if(!$this.hasClass('trigger')){
+        $this.addClass('trigger');
+        $this.html('Скрыть текст');
+
+        content.removeClass('collapsed');
+    } else {
+        $this.removeClass('trigger');
+        $this.html('Читать далее..');
+
+        content.addClass('collapsed');
+    }
+});
